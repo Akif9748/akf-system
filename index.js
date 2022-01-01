7959e50a8cac84c07b88626b6f655ea1306bd026
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2022-01-02 02:09:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+if (!process.platform=="win32") return console.log("Only for Windows for now :)")
+
+const fs = require("fs")
+const os = require("os")
+const osu = require('node-os-utils');
+
+const main = "\x1b[31m",
+    second = "\x1b[37m",
+    back="\x1b[40m",
+    build = os.release(),
+    ram = (os.totalmem() / 2 ** 20).toFixed(0);
+
+//Win ASCII and Version:
+var version,ascii;
+switch (build.substring(0, 3)) {
+    case "10.":
+        ascii= "w10"
+        version = "Windows 10 "
+        break;
+    case "6.3":
+        ascii= "w10"
+        version = "Windows 8.1 "
+        break;
+    case "6.2":
+        ascii= "w10"
+        version = "Windows 8 "
+        break;
+    case "6.1":
+        ascii= "w7"
+        version = "Windows 7 "
+        break;
+    case "5.2":
+        ascii= "w7"
+        version = "Windows XP Professional "
+        break;
+
+    case "5.1":
+        ascii= "w7"
+        version = "Windows XP "
+        break;
+
+    default:
+        version = "Not Found. "
+        ascii= "not"
+        break;
+};
+version += os.arch()
+
+
+
+console.log(back,main,"System Information:")
+//Write ASCII Blue:
+console.log("\x1b[34m",fs.readFileSync(`./ascii/${ascii}.txt`,{encoding:"utf-8"}))
+//Other Information:
+console.log(main, "Username:", second, os.userInfo().username);
+console.log(main, "OS:", second, version);
+console.log(main, "Build:", second, build);
+console.log(main, "Host:", second, osu.os.hostname());
+console.log(main, "Uptime:", second, require("./uptime.js")(os.uptime()));
+console.log(main, "CPU:", second, osu.cpu.model());
+console.log(main, "RAM:", second, ram - (os.freemem() / 2 ** 20).toFixed(0) + "MB", "/", ram + "MB");
+//Screen Information:
+require("child_process").exec("wmic path win32_VideoController get name", (error, stdout, stderr) => {
+    if (error) {
+        console.error(error.message);
+        var gpu = "Not Found";
+    }else if (stderr) {
+        console.error(stderr);
+        var gpu = "Not Found";
+    } else {
+        var gpu = stdout.split("\n")[1];
+    }
+    
+    // Normalise the result here to get the GPU name
+    console.log(back,main, "GPU:", second, gpu);
+
+});
+require("./resolution")(main, second,back);
+console.log("\x1b[0m")//RESET
---
+++ resolution.js
@@ -0,0 +1,15 @@
+var PNG = require('pngjs').PNG;
+const fs =require("fs")
+module.exports = (main,second,back) => {
+    
+    require("desktop-screenshot")("./a.png", err => {
+        if (err) console.error(err);
+        fs.createReadStream("./a.png").pipe(new PNG())
+        .on("parsed", function() {
+            console.log(back,main,"Resolution:",second,this.width+"x"+this.height)
+            fs.unlinkSync("./a.png");
+        })
+        .on("error", console.error);
+            
+    });
+};
